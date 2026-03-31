f1a0f56cd5d15b45dfe69be029533eb23a01f3f3
Jeremy Magland <jmagland@flatironinstitute.org>
2026-03-31 15:56:59 -0400

    Add blog post: Building a MIP Package with MEX Files

--- src/blog/posts/index.ts
+++ src/blog/posts/index.ts
@@ -3,8 +3,10 @@ import type { BlogPost } from "../types";
 
 import creatingChannel from "./creating-your-own-mip-channel.md?raw";
 import numblWasm from "./building-a-numbl-wasm-package.md?raw";
+import mexPackage from "./building-a-mex-package.md?raw";
 
 export const posts: BlogPost[] = [
   parseFrontmatter(creatingChannel),
   parseFrontmatter(numblWasm),
+  parseFrontmatter(mexPackage),
 ].sort((a, b) => b.date.localeCompare(a.date));
